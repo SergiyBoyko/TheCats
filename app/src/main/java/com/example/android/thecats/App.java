6b4cf69e7a1365e4587bc128a9e1f934b3ed4910
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-10-11 19:19:25 +0300

    created class for getting api

---
+++ app/src/main/java/com/example/android/thecats/App.java
@@ -0,0 +1,32 @@
+package com.example.android.thecats;
+
+import android.app.Application;
+
+import com.example.android.thecats.api.TheCatApi;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
+
+/**
+ * Created by fbrsw on 11.10.2017.
+ */
+
+public class App extends Application {
+    private static TheCatApi api;
+    private Retrofit retrofit;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://thecatapi.com") //Базовая часть адреса
+                .addConverterFactory(SimpleXmlConverterFactory.create()) //Конвертер, необходимый для преобразования JSON'а в объекты
+                .build();
+        api = retrofit.create(TheCatApi.class); //Создаем объект, при помощи которого будем выполнять запросы
+    }
+
+    public static TheCatApi getApi() {
+        return api;
+    }
+}
