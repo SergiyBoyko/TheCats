aeaf8fd74149b2096d4714a49d54917c3ad8446e
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-10-12 17:08:19 +0300

    added details activity

---
+++ app/src/main/java/com/example/android/thecats/DetailsActivity.java
@@ -0,0 +1,34 @@
+package com.example.android.thecats;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+/**
+ * Created by fbrsw on 12.10.2017.
+ */
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String id = getIntent().getStringExtra("id");
+        String url = getIntent().getStringExtra("url");
+
+        TextView titleTextView = (TextView) findViewById(R.id.image_id);
+        titleTextView.setText(id);
+
+        ImageView imageView = (ImageView) findViewById(R.id.image);
+
+        Glide
+                .with(this)
+                .load(url)
+                .into(imageView);
+    }
+}
