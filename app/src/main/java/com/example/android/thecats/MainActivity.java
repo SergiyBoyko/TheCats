1fe59b14a03c3b6cabaca9c6864978912eb1b629
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-10-17 15:43:02 +0300

    facebook sdk login implemented

--- app/src/main/java/com/example/android/thecats/MainActivity.java
+++ app/src/main/java/com/example/android/thecats/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.thecats;
 
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
@@ -13,15 +15,31 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.android.thecats.response.Category;
 import com.example.android.thecats.response.Image;
 import com.example.android.thecats.response.Response;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
@@ -31,10 +49,12 @@ public class MainActivity extends ActionBarActivity {
     private String[] mPlanetTitles;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
-    private ActionBarDrawerToggle actionBarDrawerToggle;
+//    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     private ConnectionReceiver connectionReceiver = new ConnectionReceiver();
     private boolean connected = false;
+
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,12 +72,74 @@ public class MainActivity extends ActionBarActivity {
         this.unregisterReceiver(connectionReceiver);
     }
 
+    private void initFacebookLogin() {
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+
+//        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
+
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+                String s =  "User ID: "
+                        + loginResult.getAccessToken().getUserId()
+                        + "\n" +
+                        "Auth Token: "
+                        + loginResult.getAccessToken().getToken();
+
+                Toast.makeText(MainActivity.this, s, Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onCancel() {
+
+                Toast.makeText(MainActivity.this, "cancel", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+
+                Toast.makeText(MainActivity.this, "onError " + error.toString(), Toast.LENGTH_LONG).show();
+                System.out.println("hasherrorhasherrorhasherror" + error.toString());
+            }
+        });
+
+    }
+
+    public static Bitmap getFacebookProfilePicture(String userID) throws SocketException, SocketTimeoutException, MalformedURLException, IOException, Exception
+    {
+        String imageURL;
+
+        Bitmap bitmap = null;
+        imageURL = "http://graph.facebook.com/"+userID+"/picture?type=large";
+        InputStream in = (InputStream) new URL(imageURL).getContent();
+        bitmap = BitmapFactory.decodeStream(in);
+
+        return bitmap;
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Toast.makeText(MainActivity.this, "onActivityResult " + requestCode + " " + resultCode , Toast.LENGTH_LONG).show();
+
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 
     public void tryConnect() {
         if (Utils.isOnline(this) && !connected) {
             prepareMainActivity();
             prepareNavigationDrawer();
             configToolBar();
+            initFacebookLogin();
             connected = true;
         }
 
@@ -67,82 +149,6 @@ public class MainActivity extends ActionBarActivity {
         if (!Utils.isOnline(this) || adapter == null) return false;
         ArrayList<Image> createLists = loadFirst(count, null);
         return adapter.addItems(createLists);
-    }
-
-    private void prepareNavigationDrawer() {
-//        mPlanetTitles = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"};
-        mPlanetTitles = loadCategories();
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
-
-
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-                R.layout.drawer_list_item, mPlanetTitles));
-
-        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(MainActivity.this, "Category " + mPlanetTitles[i], Toast.LENGTH_SHORT).show();
-                ArrayList<Image> images = loadFirst(50, mPlanetTitles[i]);
-                adapter.setGalleryList(images);
-                recyclerView.scrollToPosition(0);
-                mDrawerLayout.closeDrawers();
-            }
-        });
-
-    }
-
-    private class LoadCategoriesTask extends AsyncTask<Void, Void, retrofit2.Response<Response>> {
-
-        @Override
-        protected retrofit2.Response<Response> doInBackground(Void... voids) {
-            retrofit2.Response<Response> response = null;
-            try {
-                response = App.getApi().getCategories().execute();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return response;
-        }
-    }
-
-    private String[] loadCategories() {
-        Response response = null;
-        LoadCategoriesTask t = new LoadCategoriesTask();
-        t.execute();
-//        System.out.println(App.getApi().getCategories().request().toString());
-        try {
-            retrofit2.Response<Response> rez = t.get();
-            if (rez == null)
-                Toast.makeText(this, "Categories Load Failed", Toast.LENGTH_LONG).show();
-            else {
-                Toast.makeText(this, "Categories Load Success", Toast.LENGTH_LONG).show();
-                response = rez.body();
-            }
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
-
-        List<String> strings = new ArrayList<>();
-
-        for (Category c : response.getData().getCategories().getCategory()) {
-            strings.add(c.getName());
-        }
-
-        String[] stockArr = new String[strings.size()];
-        stockArr = strings.toArray(stockArr);
-
-        return stockArr;
-    }
-
-    private void configToolBar() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        toolbar.setNavigationIcon(R.drawable.categ);
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//        getSupportActionBar().setHomeButtonEnabled(true);
-
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.app_name, R.string.app_name);
     }
 
     private void prepareMainActivity() {
@@ -184,12 +190,90 @@ public class MainActivity extends ActionBarActivity {
                 if (pastVisibleItems + visibleItemCount >= totalItemCount) {
                     //End of list
                     loadMore(adapter, 50);
-//                    Toast.makeText(MainActivity.this, "Adding", Toast.LENGTH_LONG).show();
                 }
 
             }
         });
     }
+
+
+    private void prepareNavigationDrawer() {
+        mPlanetTitles = loadCategories();
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mPlanetTitles));
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                try {
+                    Toast.makeText(MainActivity.this, "Category " + mPlanetTitles[i], Toast.LENGTH_SHORT).show();
+                    ArrayList<Image> images = loadFirst(50, mPlanetTitles[i]);
+                    adapter.setGalleryList(images);
+                    recyclerView.scrollToPosition(0);
+                    mDrawerLayout.closeDrawers();
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity.this, "Server error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    private class LoadCategoriesTask extends AsyncTask<Void, Void, retrofit2.Response<Response>> {
+
+        @Override
+        protected retrofit2.Response<Response> doInBackground(Void... voids) {
+            retrofit2.Response<Response> response = null;
+            try {
+                response = App.getApi().getCategories().execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
+    }
+
+    private String[] loadCategories() {
+        Response response = null;
+        LoadCategoriesTask t = new LoadCategoriesTask();
+        t.execute();
+        try {
+            retrofit2.Response<Response> rez = t.get();
+            if (rez == null)
+                Toast.makeText(this, "Categories Load Failed", Toast.LENGTH_LONG).show();
+            else {
+                response = rez.body();
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        List<String> strings = new ArrayList<>();
+
+        for (Category c : response.getData().getCategories().getCategory()) {
+            strings.add(c.getName());
+        }
+
+        String[] stockArr = new String[strings.size()];
+        stockArr = strings.toArray(stockArr);
+
+        return stockArr;
+    }
+
+    private void configToolBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.categ);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setHomeButtonEnabled(true);
+
+        new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.app_name, R.string.app_name);
+    }
+
 
     private class LoadImagesTask extends AsyncTask<String, Void, retrofit2.Response<com.example.android.thecats.response.Response>> {
 
@@ -217,7 +301,6 @@ public class MainActivity extends ActionBarActivity {
             retrofit2.Response<com.example.android.thecats.response.Response> rez = t.get();
             if (rez == null) Toast.makeText(this, "Error loading", Toast.LENGTH_LONG).show();
             else {
-//                Toast.makeText(this, "Loaded", Toast.LENGTH_LONG).show();
                 response = rez.body();
             }
         } catch (InterruptedException | ExecutionException e) {
